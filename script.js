6defa7d52ccd534ee758d4bdd78b97548fce88f3
muxmasterman <muxmasterman981@gmail.com>
2023-12-11 19:54:33 +0100

    b

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
     var video = document.getElementById('videoLoop');
     video.play();
+
+    // Versucht, in den Vollbildmodus zu wechseln
+    if (video.requestFullscreen) {
+        video.requestFullscreen();
+    } else if (video.mozRequestFullScreen) { /* Firefox */
+        video.mozRequestFullScreen();
+    } else if (video.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+        video.webkitRequestFullscreen();
+    } else if (video.msRequestFullscreen) { /* IE/Edge */
+        video.msRequestFullscreen();
+    }
 });
